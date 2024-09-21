597bfc7468de51d0fcbe48e1012c620d9d572bdd
ym-y-u <3005y.yumi@gmail.com>
2024-09-21 09:36:48 +0900

    会員側の認証を実装

---
+++ database/migrations/2024_09_21_091427_add_columns_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('kana');
+            $table->string('postal_code');
+            $table->string('address');
+            $table->string('phone_number');
+            $table->date('birthday')->nullable();
+            $table->string('occupation')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('kana');
+            $table->dropColumn('postal_code');
+            $table->dropColumn('address');
+            $table->dropColumn('phone_number');
+            $table->dropColumn('birthday');
+            $table->dropColumn('occupation');
+        });
+    }
+};
